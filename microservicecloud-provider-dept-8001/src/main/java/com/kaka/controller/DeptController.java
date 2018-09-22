e9479346ab2458ce11299ae5daf81d5be2651abe
Richard <289799074@qq.com>
2018-09-22 13:01:55 +0800

    dept model crud test...

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/kaka/controller/DeptController.java
@@ -0,0 +1,62 @@
+package com.kaka.controller;
+
+import java.util.List;
+
+import com.kaka.entities.Dept;
+import com.kaka.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class DeptController
+{
+	@Autowired
+	private DeptService service;
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	public boolean add(@RequestBody Dept dept)
+	//@requestBody可以将请求体中的JSON字符串绑定到相应的bean上，当然，也可以将其分别绑定到对应的字符串上
+	{
+		return service.add(dept);
+	}
+
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable("id") Long id)
+	{
+		return service.get(id);
+	}
+
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list()
+	{
+		return service.list();
+	}
+
+	
+//	@Autowired
+//	private DiscoveryClient client;
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
+
+}
---
+++ microservicecloud-provider-dept-8001/src/main/java/com/kaka/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kaka.service.impl;
+
+import java.util.List;
+
+import com.kaka.dao.DeptDao;
+import com.kaka.entities.Dept;
+import com.kaka.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
